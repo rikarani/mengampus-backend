c2e07d290648dee18e28287bc7099f778bd0802a
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-12-04 18:15:29 +0700

    delete

--- prisma/migrations/20251203052958_add_prodi_fields_to_users_table/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `prodi` to the `users` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `users` ADD COLUMN `prodi` VARCHAR(191) NOT NULL,
-    MODIFY `email_verified` BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20251203150350_remove_time_field_in_events_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `time` on the `events` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `events` DROP COLUMN `time`;
