1eb562df2abcd2bd8d6ee887f38ab1069dbb513b
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-12-06 01:54:29 +0700

    delete file

--- src/schemas/auth/login.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const loginSchema = z.object({
-  nim: z.string().min(11),
-  password: z.string().min(8),
-  role: z.enum(["admin", "user"]),
-});
-
-export type LoginSchema = z.infer<typeof loginSchema>;
