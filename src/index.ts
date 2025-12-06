7d4832f5cdeac701bed7684b8f8efa6ca5a027de
rikarani <46773591+rikarani@users.noreply.github.com>
2025-12-06 17:50:54 +0700

    Add category and event routers to the main application

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ import express from "express";
 import { auth } from "@/lib/auth";
 import { toNodeHandler } from "better-auth/node";
 
+import { categoryRouter } from "./routes/category";
+import { eventRouter } from "./routes/event";
+
 const app = express();
 const port = 4000;
 
@@ -18,6 +21,9 @@ app.use(express.json());
 app.get("/", (request, response) => {
   return response.json({ message: "Halo Mengampus API v1" });
 });
+
+app.use("/categories", categoryRouter);
+app.use("/events", eventRouter);
 
 app.listen(port, () => {
   console.log(`Server Ready di Port ${port}`);
