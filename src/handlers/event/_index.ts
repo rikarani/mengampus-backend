d0cabaddde2c1056d753c80de1672ce69e75417a
rikarani <46773591+rikarani@users.noreply.github.com>
2025-12-06 17:51:06 +0700

    Add event index handler to retrieve all events

---
+++ src/handlers/event/_index.ts
@@ -0,0 +1,15 @@
+import type { Request, Response } from "express";
+
+import { prisma } from "@/prisma/prisma";
+import type { EventModel } from "../../../prisma/generated/models";
+
+import type { API } from "@/types";
+
+export async function index(request: Request, response: Response<API<EventModel[]>>) {
+  const events = await prisma.event.findMany();
+
+  return response.status(200).json({
+    success: true,
+    data: events,
+  });
+}
