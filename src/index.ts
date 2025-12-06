afdb7a6b6e2fb41590b995062d254a468655efd5
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-12-06 09:17:02 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,21 @@ import "dotenv/config";
 import cors from "cors";
 import express from "express";
 
-import { login, register } from "@/handlers/auth";
+import { auth } from "@/lib/auth";
+import { toNodeHandler } from "better-auth/node";
 
 const app = express();
 const port = 4000;
 
 app.use(cors());
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
 app.use(express.json());
 
 app.get("/", (request, response) => {
   return response.json({ message: "Halo Mengampus API v1" });
 });
-
-app.post("/login", login);
-app.post("/register", register);
 
 app.listen(port, () => {
   console.log(`Server Ready di Port ${port}`);
