827ce6f93dc7d835b5e9e67b716194c21e0ab082
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-12-06 09:17:24 +0700

    Create auth.ts

---
+++ src/lib/auth.ts
@@ -0,0 +1,74 @@
+import { betterAuth } from "better-auth";
+import { expo } from "@better-auth/expo";
+
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "@/prisma/prisma";
+
+export const auth = betterAuth({
+  plugins: [expo()],
+  database: prismaAdapter(prisma, {
+    provider: "mysql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false,
+  },
+  advanced: {
+    database: {
+      generateId: "uuid",
+    },
+  },
+  trustedOrigins: ["exp://", "mengampus://"],
+  account: {
+    modelName: "Account",
+    fields: {
+      userId: "user_id",
+      accountId: "account_id",
+      providerId: "provider_id",
+      accessToken: "access_token",
+      refreshToken: "refresh_token",
+      idToken: "id_token",
+      accessTokenExpiresAt: "access_token_expires_at",
+      refreshTokenExpiresAt: "refresh_token_expires_at",
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  },
+  session: {
+    modelName: "Session",
+    fields: {
+      userId: "user_id",
+      ipAddress: "ip_address",
+      userAgent: "user_agent",
+      expiresAt: "expires_at",
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  },
+  user: {
+    modelName: "User",
+    fields: {
+      emailVerified: "email_verified",
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+    additionalFields: {
+      nim: {
+        type: "string",
+        required: true,
+      },
+      prodi: {
+        type: "string",
+        required: true,
+      },
+    },
+  },
+  verification: {
+    modelName: "Verification",
+    fields: {
+      expiresAt: "expires_at",
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  },
+});
