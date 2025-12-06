75e4940f1009a707a759adc1ca7f787180b6f5c3
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-12-06 09:17:10 +0700

    Create migration.sql

---
+++ prisma/migrations/20251205184659_init/migration.sql
@@ -0,0 +1,71 @@
+-- CreateTable
+CREATE TABLE `accounts` (
+    `id` VARCHAR(191) NOT NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+    `account_id` TEXT NOT NULL,
+    `provider_id` TEXT NOT NULL,
+    `access_token` TEXT NULL,
+    `refresh_token` TEXT NULL,
+    `id_token` TEXT NULL,
+    `access_token_expires_at` DATETIME(3) NULL,
+    `refresh_token_expires_at` DATETIME(3) NULL,
+    `scope` TEXT NULL,
+    `password` TEXT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    INDEX `accounts_user_id_idx`(`user_id`(191)),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `sessions` (
+    `id` VARCHAR(191) NOT NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `ip_address` TEXT NULL,
+    `user_agent` TEXT NULL,
+    `expires_at` DATETIME(3) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    INDEX `sessions_user_id_idx`(`user_id`(191)),
+    UNIQUE INDEX `sessions_token_key`(`token`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `users` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` TEXT NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `email_verified` BOOLEAN NOT NULL DEFAULT false,
+    `nim` VARCHAR(191) NOT NULL,
+    `prodi` TEXT NOT NULL,
+    `role` VARCHAR(191) NOT NULL DEFAULT 'user',
+    `image` TEXT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `users_email_nim_key`(`email`, `nim`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `verifications` (
+    `id` VARCHAR(191) NOT NULL,
+    `identifier` TEXT NOT NULL,
+    `value` TEXT NOT NULL,
+    `expires_at` DATETIME(3) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    INDEX `verifications_identifier_idx`(`identifier`(191)),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `accounts` ADD CONSTRAINT `accounts_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `sessions` ADD CONSTRAINT `sessions_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
