43d305b470d2bf10e1f44a7b8941f05866bbd03e
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-12-07 22:08:15 +0700

    Update _index.ts

--- src/handlers/event/_index.ts
+++ src/handlers/event/_index.ts
@@ -1,11 +1,11 @@
 import type { Request, Response } from "express";
 
 import { prisma } from "@/prisma/prisma";
-import type { EventModel } from "../../../prisma/generated/models";
+import type { EventModel, CategoryModel } from "../../../prisma/generated/models";
 
 import type { API } from "@/types";
 
-export async function index(request: Request, response: Response<API<EventModel[]>>) {
+export async function index(request: Request, response: Response<API<(EventModel & { category: CategoryModel })[]>>) {
   const events = await prisma.event.findMany({
     include: {
       category: true,
