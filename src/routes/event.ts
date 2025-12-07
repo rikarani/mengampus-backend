9dc390e67fec3f5a86032db4b1102c5f15d32ee4
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-12-07 22:08:24 +0700

    Update event.ts

--- src/routes/event.ts
+++ src/routes/event.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
-import { index, create } from "@/handlers/event";
+import { index, create, deleteEvent } from "@/handlers/event";
 
 const router = Router();
 
 router.get("/", index);
 router.post("/create", create);
+router.post("/delete/:id", deleteEvent);
 
 export { router as eventRouter };
