bf0ead6d1933bc2ae24e05fe88c0b10b397f1e3e
rikarani <46773591+rikarani@users.noreply.github.com>
2025-12-08 17:38:29 +0700

    Add auth middleware to categories route for enhanced security

--- src/index.ts
+++ src/index.ts
@@ -9,20 +9,27 @@ import { toNodeHandler } from "better-auth/node";
 import { categoryRouter } from "./routes/category";
 import { eventRouter } from "./routes/event";
 
+import { authMiddleware } from "./middlewares/auth";
+
 const app = express();
 const port = 4000;
 
-app.use(cors());
+app.use(
+  cors({
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
+app.get("/", (_, response) => {
   return response.json({ message: "Halo Mengampus API v1" });
 });
 
-app.use("/categories", categoryRouter);
+app.use("/categories", authMiddleware, categoryRouter);
 app.use("/events", eventRouter);
 
 app.listen(port, () => {
