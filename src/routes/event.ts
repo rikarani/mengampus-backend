da1b7c2db1157994db7e51762d1f4ab411fd691b
rikarani <46773591+rikarani@users.noreply.github.com>
2025-12-08 17:39:03 +0700

    Add event detail handler to retrieve event information by ID

--- src/routes/event.ts
+++ src/routes/event.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
-import { index, create, deleteEvent } from "@/handlers/event";
+import { index, create, detail, deleteEvent } from "@/handlers/event";
 
 const router = Router();
 
 router.get("/", index);
 router.post("/create", create);
+router.get("/detail/:id", detail);
 router.post("/delete/:id", deleteEvent);
 
 export { router as eventRouter };
