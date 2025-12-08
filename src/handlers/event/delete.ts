dda77ab0d572cbfcf6f3f60b2adf034d75170f81
rikarani <46773591+rikarani@users.noreply.github.com>
2025-12-08 17:38:46 +0700

    Refactor delete event handler to streamline response structure and remove redundant imports

--- src/handlers/event/delete.ts
+++ src/handlers/event/delete.ts
@@ -1,8 +1,7 @@
+import type { API } from "@/types";
 import type { Request, Response } from "express";
 
 import { Prisma, prisma } from "@/prisma/prisma";
-
-import type { API } from "@/types";
 
 export async function deleteEvent(request: Request<{ id: string }>, response: Response<API>) {
   const { id } = request.params;
@@ -14,18 +13,16 @@ export async function deleteEvent(request: Request<{ id: string }>, response: Re
 
     return response.status(200).json({
       success: true,
-      data: {
-        message: "Berhasil Menghapus Event",
-      },
+      message: "Berhasil Menghapus Event",
+      data: null,
     });
   } catch (error) {
     if (error instanceof Prisma.PrismaClientKnownRequestError) {
       if (error.code === "P2025") {
         return response.status(404).json({
           success: false,
-          data: {
-            message: "Event tidak ditemukan",
-          },
+          message: "Event tidak ditemukan",
+          errorCode: error.code,
         });
       }
     }
