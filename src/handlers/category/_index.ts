1b5f61542dfa5fd730229d379b7362d939933dab
rikarani <46773591+rikarani@users.noreply.github.com>
2025-12-08 17:38:35 +0700

    Refactor index handler to include session authentication logic

--- src/handlers/category/_index.ts
+++ src/handlers/category/_index.ts
@@ -2,9 +2,24 @@ import type { API } from "@/types";
 import type { Request, Response } from "express";
 import type { Category } from "../../../prisma/generated/client";
 
+import { fromNodeHeaders } from "better-auth/node";
+
+import { auth } from "@/lib/auth";
 import { prisma } from "@/prisma/prisma";
 
-export async function index(_: Request, response: Response<API<Category[]>>) {
+export async function index(request: Request, response: Response<API<Category[]>>) {
+  // const session = await auth.api.getSession({
+  //   headers: fromNodeHeaders(request.headers),
+  // });
+
+  // if (!session) {
+  //   return response.status(401).json({
+  //     success: false,
+  //     message: "Unauthorized",
+  //     errorCode: "401",
+  //   });
+  // }
+
   const categories = await prisma.category.findMany();
 
   return response.status(200).json({
