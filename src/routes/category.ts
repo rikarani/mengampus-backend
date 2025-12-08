7c0bcf616c4e532ebf97beab45feed21072bbf1d
rikarani <46773591+rikarani@users.noreply.github.com>
2025-12-08 11:57:05 +0700

    Add update and delete category routes to category router

--- src/routes/category.ts
+++ src/routes/category.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
-import { index, create } from "@/handlers/category";
+import { index, create, update, deleteCategory } from "@/handlers/category";
 
 const router = Router();
 
 router.get("/", index);
 router.post("/create", create);
+router.patch("/update/:id", update);
+router.delete("/delete/:id", deleteCategory);
 
 export { router as categoryRouter };
