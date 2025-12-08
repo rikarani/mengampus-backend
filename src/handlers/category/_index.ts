bfae3719753986ad9c47853a90ddc84690a6ba56
rikarani <46773591+rikarani@users.noreply.github.com>
2025-12-08 11:56:31 +0700

    Refactor category index handler to improve type imports and response structure

--- src/handlers/category/_index.ts
+++ src/handlers/category/_index.ts
@@ -1,15 +1,15 @@
+import type { API } from "@/types";
 import type { Request, Response } from "express";
+import type { Category } from "../../../prisma/generated/client";
 
 import { prisma } from "@/prisma/prisma";
-import type { CategoryModel } from "../../../prisma/generated/models";
 
-import type { API } from "@/types";
-
-export async function index(request: Request, response: Response<API<CategoryModel[]>>) {
+export async function index(_: Request, response: Response<API<Category[]>>) {
   const categories = await prisma.category.findMany();
 
   return response.status(200).json({
     success: true,
+    message: "Berhasil Mengambil Kategori",
     data: categories,
   });
 }
