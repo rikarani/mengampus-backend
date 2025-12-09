4201812672a81aa03c38bc07d5cbe2b534040b49
rikarani <46773591+rikarani@users.noreply.github.com>
2025-12-09 16:28:50 +0700

    Add edit route for events and update delete method to use DELETE verb

--- src/routes/event.ts
+++ src/routes/event.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 
-import { index, create, detail, deleteEvent } from "@/handlers/event";
+import { index, create, detail, edit, deleteEvent } from "@/handlers/event";
 
 const router = Router();
 
 router.get("/", index);
 router.post("/create", create);
 router.get("/detail/:id", detail);
-router.post("/delete/:id", deleteEvent);
+router.patch("/edit/:id", edit);
+router.delete("/delete/:id", deleteEvent);
 
 export { router as eventRouter };
